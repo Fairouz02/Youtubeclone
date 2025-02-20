db943efe6f45f87378fb8fc2d3c9121808b92812
Fairouz02 <md.fairouz02@gmail.com>
2025-02-20 10:44:02 +0800

    Project Setup
    D1- Download all dependencies

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+
+export default function Home() {
+  return (
+    <div>
+      <p className="font-bold text-rose-500">
+        <Button variant="destructive">
+          Click me!
+        </Button>
+      </p>
+    </div>
+  )
+}
