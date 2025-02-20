fe82adc60d49c9d60e218626943aec6643c04f6b
Fairouz02 <md.fairouz02@gmail.com>
2025-02-20 18:38:19 +0800

    DB configuration
    Day 1.3 Configured DB

--- src/modules/home/ui/components/home-sidebar/main-section.tsx
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -3,6 +3,7 @@
 "use client"
 
 import { SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
+import { useAuth, useClerk } from "@clerk/nextjs"
 import { FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react"
 import Link from "next/link"
 
@@ -22,13 +23,19 @@ const items = [{
 }]
 
 export const MainSection = () => {
+    const clerk = useClerk()
+    const { isSignedIn } = useAuth()
     return (
         <SidebarGroup>
             <SidebarGroupContent>
                 <SidebarMenu>
                     {items.map((item) => (
                         <SidebarMenuItem key={item.title}> 
-                            <SidebarMenuButton tooltip={item.title} asChild isActive={false} onClick={() => {}}>
+                            <SidebarMenuButton tooltip={item.title} asChild isActive={false} onClick={(e) => 
+                                { if (!isSignedIn && item.auth){
+                                    e.preventDefault()
+                                    return clerk.openSignIn()
+                                }}}>
                                 <Link href={item.url} className="flex items-center gap-4">
                                     <item.icon />
                                     <span className="text-sm">
